24213b277197d18eaf3d30be50cca0e9b0ce0e97
Vagner <vagner-bw@hotmail.com>
2022-07-08 16:27:17 -0300

    inversão do sinal > para o <

--- exerciciosJava/Exercicios01/src/exerciciosAula_03/ex2.java
+++ exerciciosJava/Exercicios01/src/exerciciosAula_03/ex2.java
@@ -17,9 +17,9 @@ public class ex2 {//Faça um programa que entre com três números e coloque em ord
 		System.out.println("Digite o Terceiro numero: ");
 		c = crescente.nextDouble();
 		
-		if ((a > b) && (a > c) && (b > c)) {
+		if ((a < b) && (a < c) && (b < c)) {
 			System.out.println("o valor Crescente é:1°  "+a+" | "+"2°: "+b+" | "+"3°: "+c);
-		}else if((b > a) && (b > c) && (a > c)) {
+		}else if((b < a) && (b < c) && (a < c)) {
 			System.out.println("o valor Crescente é:1°  "+b+" | "+"2°: "+a+" | "+"3°:  "+c);
 		}else {
 			System.out.println("o valor Crescente é:1°  "+c+" | "+"2°: "+b+" | "+"3°:  "+a);
